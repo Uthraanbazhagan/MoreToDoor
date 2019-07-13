ee4d040807f13d765a47484090ca70fb9b29fc78
Saurav Sanyal <sanyal.s271@gmail.com>
2019-07-13 10:44:29 -0700

    First commit

---
+++ More2Door/src/main/java/com/lti/more2door/More2DoorApplication.java
@@ -0,0 +1,13 @@
+package com.lti.more2door;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class More2DoorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(More2DoorApplication.class, args);
+	}
+
+}
